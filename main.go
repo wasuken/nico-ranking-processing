d73dcf832b290f07fa7114668c447ec66498cdac
wasuken <wevorence@gmail.com>
2019-09-26 19:57:22 +0900

    implement graph.

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/mmcdole/gofeed"
 	"github.com/urfave/cli"
+	"github.com/wcharczuk/go-chart"
 	"os"
 	"os/user"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
 	"unicode"
+	"unicode/utf8"
 )
 
 func rankingCsv(filepath string) {
@@ -34,16 +37,35 @@ func rankingCsv(filepath string) {
 	writer.Flush()
 }
 
-func getLiverNames(url string) ([]string, error) {
+func getLiverNames(url string) (map[string]string, error) {
 	doc, err := goquery.NewDocument(url)
-	var namesAry []string
+	namesMap := map[string]string{}
 	var regxNewline = regexp.MustCompile(`\r\n|\r|\n`)
-	doc.Find(".roundcorner span").Each(func(index int, s *goquery.Selection) {
+	doc.Find(".roundcorner").Each(func(index int, s *goquery.Selection) {
 		if strings.TrimSpace(s.Text()) != "" {
-			namesAry = append(namesAry, strings.TrimSpace(regxNewline.ReplaceAllString(s.Text(), "")))
+			href, _ := s.Find("a").Attr("href")
+			href = strings.Split(href, "/")[len(strings.Split(href, "/"))-2]
+			namesMap[strings.TrimSpace(regxNewline.ReplaceAllString(s.Find("span").Text(), ""))] = href
 		}
 	})
-	return namesAry, err
+	return namesMap, err
+}
+
+func getLiverAliasMapFromCsv(filepath string) [][]string {
+	f, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	c := csv.NewReader(f)
+	result := [][]string{}
+	crec, er := c.ReadAll()
+	if er != nil {
+		panic(er)
+	}
+	for _, r := range crec[1:] {
+		result = append(result, []string{r[0], r[1], r[2]})
+	}
+	return result
 }
 
 func inHiraganaAndKatakana(str string) bool {
@@ -72,7 +94,7 @@ func splitHiraganaAndKatakana(str string) (string, string) {
 	return a, b
 }
 
-func saveLiverNamesToCsv(headers, names []string) {
+func saveLiverNamesToCsv(headers []string, nm map[string]string) {
 	_, err := os.Stat("liverNamesAndAlias.csv")
 	if err == nil {
 		e := os.Remove("liverNamesAndAlias.csv")
@@ -89,24 +111,96 @@ func saveLiverNamesToCsv(headers, names []string) {
 	defer w.Flush()
 	w.Write(headers)
 	// insert to fullname
-	for _, name := range names {
-		w.Write([]string{name, name})
+	for k, v := range nm {
+		w.Write([]string{k, k, v})
 		// processing of retention point
-		if strings.Index(name, "・") >= 0 {
-			sp_names := strings.Split(name, "・")
-			w.Write([]string{name, sp_names[0]})
-			w.Write([]string{name, sp_names[1]})
-		} else if inHiraganaAndKatakana(name) {
-			a, b := splitHiraganaAndKatakana(name)
-			w.Write([]string{name, a})
-			w.Write([]string{name, b})
+		if strings.Index(k, "・") >= 0 {
+			sp_names := strings.Split(k, "・")
+			w.Write([]string{k, sp_names[0], v})
+			w.Write([]string{k, sp_names[1], v})
+		} else if inHiraganaAndKatakana(k) && utf8.RuneCountInString(k) <= 3 {
+			continue
+		} else if inHiraganaAndKatakana(k) {
+			a, b := splitHiraganaAndKatakana(k)
+			w.Write([]string{k, a, v})
+			w.Write([]string{k, b, v})
 		}
 	}
 
 	// I want to be first and last name separated if possible...!
 	// but I can't it.
 	// I want a force...!
+}
 
+func determineWhatLiverFromString(str string, nameMap map[string]string) string {
+	for k, _ := range nameMap {
+		if strings.Index(str, k) >= 0 {
+			return k
+		}
+	}
+	return "other"
+}
+
+func createChartVals(pathquerys []string, namesCsvpath string) []chart.Value {
+	csvList := getLiverAliasMapFromCsv(namesCsvpath)
+	nameAliasMap := map[string]string{}
+	for _, r := range csvList {
+		nameAliasMap[r[0]] = r[1]
+	}
+	nameRomajiMap := map[string]string{}
+	for _, r := range csvList {
+		nameRomajiMap[r[0]] = r[2]
+	}
+	nameCnt := map[string]int{}
+	vals := []chart.Value{}
+	for _, f := range pathquerys {
+		f, _ := os.Open(f)
+		defer f.Close()
+		c := csv.NewReader(f)
+		allRec, er := c.ReadAll()
+		if er != nil {
+			panic(er)
+		}
+		for _, row := range allRec[1:] {
+			name := determineWhatLiverFromString(row[0], nameAliasMap)
+			if name != "other" {
+				nameCnt[nameRomajiMap[name]] += 1
+			} else {
+				nameCnt[name] += 1
+			}
+		}
+		allRec = [][]string{}
+	}
+	for k, v := range nameCnt {
+		vals = append(vals, chart.Value{Value: float64(v), Label: k})
+	}
+	return vals
+}
+
+func graph(vals []chart.Value, savepath, title string) {
+	bar := chart.BarChart{
+		Title: title,
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top: 40,
+			},
+		},
+		Width:  1500,
+		Height: 512,
+		XAxis:  chart.StyleShow(),
+		YAxis: chart.YAxis{
+			Style: chart.StyleShow(),
+			// TickStyle: chart.Style{
+			//  TextRotationDegrees: 45.0,
+			// },
+			NameStyle: chart.Style{Show: true},
+		},
+		Bars: vals,
+	}
+
+	f, _ := os.Create(savepath)
+	defer f.Close()
+	bar.Render(chart.PNG, f)
 }
 func main() {
 	app := cli.NewApp()
@@ -120,12 +214,12 @@ func main() {
 			Aliases: []string{},
 			Usage:   "get liver names",
 			Action: func(c *cli.Context) error {
-				names, e := getLiverNames("https://nijisanji.ichikara.co.jp/member/")
+				nm, e := getLiverNames("https://nijisanji.ichikara.co.jp/member/")
 				if e != nil {
 					panic("failed get name")
 				}
-				for _, name := range names {
-					fmt.Println(name)
+				for k, v := range nm {
+					fmt.Println(k + ":" + v)
 				}
 				return nil
 			},
@@ -135,11 +229,11 @@ func main() {
 			Aliases: []string{},
 			Usage:   "get liver names and save csv",
 			Action: func(c *cli.Context) error {
-				names, e := getLiverNames("https://nijisanji.ichikara.co.jp/member/")
+				nm, e := getLiverNames("https://nijisanji.ichikara.co.jp/member/")
 				if e != nil {
 					panic("failed get name")
 				}
-				saveLiverNamesToCsv([]string{"name", "alias"}, names)
+				saveLiverNamesToCsv([]string{"name", "alias", "romaji"}, nm)
 				return nil
 			},
 		},
@@ -158,7 +252,39 @@ func main() {
 			Aliases: []string{},
 			Usage:   "create graph",
 			Action: func(c *cli.Context) error {
-				fmt.Println("Under construction")
+				querypaths := []string{}
+				for _, path := range c.Args()[1 : c.NArg()-2] {
+					usr, _ := user.Current()
+					querypath := strings.Replace(path, "~", usr.HomeDir, 1)
+					querypaths = append(querypaths, querypath)
+				}
+				namesCsvpath := c.Args().Get(c.NArg() - 1)
+				vals := createChartVals(querypaths, namesCsvpath)
+				sort.Slice(vals, func(i, j int) bool {
+					return vals[i].Value > vals[j].Value
+				})
+				total := 0.0
+				for _, v := range vals {
+					if v.Label != "other" {
+						total += v.Value
+					}
+				}
+				vals = vals[0:11]
+				delptr := -1
+				for i, v := range vals {
+					if v.Label == "other" {
+						delptr = i
+					} else {
+						v_s := fmt.Sprintf("%f", v.Value)
+						// v_ratio := fmt.Sprintf("%f", ((v.Value / total) * 100))
+						// vals[i].Label += "\n" + v_ratio + "%"
+						fmt.Println(v.Label + ":" + v_s)
+					}
+				}
+				if delptr >= 0 {
+					vals = append(vals[:delptr], vals[delptr+1:]...)
+				}
+				graph(vals, "result.png", "liver cuts top 5")
 				return nil
 			},
 		},
